b371b00524980de4e7805e88f837bc936131613a
perry <User@Tipzee-Aspire.(none)>
2020-04-30 19:53:05 +0800

    Add store method

--- src/modules/user/controllers/user.controller.js
+++ src/modules/user/controllers/user.controller.js
@@ -1,11 +1,22 @@
+import Service from '../services/user.service';
+
 class UserController {
+
+    constructor() {
+        this._service = new Service();
+        this.store = this.store.bind(this);
+    }
 
     index(req, res) {
         return res.send('test');
     }
 
     store(req, res) {
-
+        //
+        return this._service
+            .save(req.body)
+            .then(result => res.send(result))
+            .catch(err => res.send(err));
     }
 
     show(req, res) {
