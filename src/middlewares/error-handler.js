6516b8c14a9640bae1b450a53a0b26f0ec2d2e95
perry <User@Tipzee-Aspire.(none)>
2020-05-01 18:31:54 +0800

    Refactor error handler

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -1,14 +1,23 @@
-class Handler {
+import { failed } from "../libs/response/response";
+import { Error } from "mongoose";
+import * as StatusCodes from "http-status-codes";
+import { Errors } from "../libs/error/error";
 
-    parse(err, req, res, next) {
-        //
-        if (res.headersSent) {
-            return next(err);
-        }
-        
-        res.status(500);
-        res.json({ error: err });
+function error(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err);
     }
+
+    if (err instanceof Error.ValidationError) {
+        return failed(
+            res, 
+            Errors.Mongoose.toJSON(err), 
+            'Validation Failed', 
+            StatusCodes.UNPROCESSABLE_ENTITY
+        );
+    }    
+
+    failed(res, err, 'Error');
 }
 
-export let ErrorHandler = new Handler();
+export {error as ErrorHandler};
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { ErrorHandler } from "./middlewares/error-handler";
 const app = express();
 const port = 3000;
 
-
+app.use(express.json());
 
 const dbConStr = 'mongodb://localhost:27017/prymag-timetracker';
 const dbOpts = {
@@ -19,7 +19,7 @@ const db = new MongooseDB(dbConStr, dbOpts);
 db.start()
 .then((res) => {
     routes.load(app);
-    app.use(ErrorHandler.parse);
+    app.use(ErrorHandler);
     app.get('/', (req, res) => res.send('Hello World!'));
     app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 }).catch (err => {
