34cb08e9c101a69f70bffcbf98fc614fbaceb210
perry <User@Tipzee-Aspire.(none)>
2020-05-16 08:15:37 +0800

    mongoose error parser test

---
+++ tests/framework/libs/error-parser/mongoose.test.js
@@ -0,0 +1,25 @@
+import UserModel from "@framework/models/user.model";
+import { Error } from "mongoose";
+import parse from "@framework/libs/error-parser/mongoose";
+
+describe('Libs/error-parser/mongoose', () => {
+
+    it('Should parse mongoose error', () => {
+        //
+        const error = new Error.ValidationError(new UserModel());
+        error.errors = {
+            password: new Error.ValidatorError({message: 'Error message', type: 'error-type', path: 'password'})
+        };
+
+        const expected = [{
+            field: 'password',
+            type: 'error-type',
+            message: 'Error message'
+        }];
+
+        const result = parse(error);
+
+        expect(result).toEqual(expected);
+    });
+
+});
