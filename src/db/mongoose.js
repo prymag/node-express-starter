6da2a1fef1022b21b661aa0113f30bf14f5501c0
perry <User@Tipzee-Aspire.(none)>
2020-05-19 09:43:43 +0800

    cleanup mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,32 +3,23 @@ import mongoose from 'mongoose';
 class MongooseDB {
 
     constructor(connectionStr, opts) {
-        this._connection = false;
         this._conectionStr = connectionStr;
         this._opts = opts;
     }
 
     start(){
         //
-        if(this._connection) {
-            return Promise.resolve(this._connection);
-        }
-        this._connection = mongoose.connect(this._conectionStr, this._opts);
-
-        return Promise.resolve(this._connection);
+        const con = mongoose.connect(this._conectionStr, this._opts);
+        return Promise.resolve(con);
+        //
     }
 
-    stop(callback) {
+    stop(cb) {
         //
-        if (this._connection) {
-            _connection.disconnect();
-            _connection = undefined;
-            callback();
-            return Promise.resolve(true);
-        }
-
-        callback();
-        return Promise.reject('Not connected');
+        const result = mongoose.disconnect();
+        cb();
+        return result;
+        //
     }
 }
 
